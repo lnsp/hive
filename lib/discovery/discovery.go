7930485e2ea0084657b3fb30399b38afea1f93cd
Lennart Espe <lennart@mooxmirror.io>
2017-01-22 19:23:52 +0100

    lib: rename lib/discover to lib/discovery

---
+++ lib/discovery/discovery.go
@@ -0,0 +1,25 @@
+package discovery
+
+import "github.com/lnsp/hive/lib/service"
+
+type Discovery struct {
+	services map[string]service.Service
+}
+
+func (discovery Discovery) Register(service service.Service) {
+	discovery.services[service.Name] = service
+}
+
+func (discovery Discovery) Retrieve(name string) service.Service {
+	return discovery.services[name]
+}
+
+func (discovery Discovery) Send(name string, method string, request interface{}) (interface{}, error) {
+	return discovery.services[name].Send(method, request)
+}
+
+func New() Discovery {
+	return Discovery{
+		make(map[string]service.Service, 0),
+	}
+}
